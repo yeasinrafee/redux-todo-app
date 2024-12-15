fcc1c977b95a17dad8cb70fef301d8a500920199
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-12-15 14:03:43 +0600

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1>My Todo Application</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
